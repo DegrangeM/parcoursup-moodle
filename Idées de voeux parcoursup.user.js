2346c4b5087e3db5f20bdaec91f331b11b87540f
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-09-12 14:05:47 +0200

    Add files via upload

---
+++ Idées de voeux parcoursup.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Idées de voeux parcoursup
+// @version      1
+// @description  Permet d'ajouter des idées de voeux parcoursup
+// @author       Degrange
+// @match        https://dossier.parcoursup.fr/Candidats/public/fiches/afficherFicheFormation*
+// @match        https://moodle.monlycee.net/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const URL = 'https://moodle.monlycee.net/mod/data/edit.php?d=37';
+    
+    if(location.host === 'dossier.parcoursup.fr') {
+        const boutonImprimer = document.getElementById('btn-imprimer') || document.getElementById('btn-imprimer-2');
+        const boutonFavoris = document.createElement('button');
+        boutonImprimer.parentNode.insertBefore(boutonFavoris, boutonImprimer)
+        const imageFavoris = document.createElement('img');
+        imageFavoris.src = 'https://www.parcoursup.fr/images/Pictogramme/picto-parchemin.svg';
+        boutonFavoris.appendChild(imageFavoris)
+        boutonFavoris.className = 'btn btn-bordure-orange btn-icone-fiche btn-impression pr-1 mb-3 ml-2'
+        imageFavoris.classList.add('icon')
+        imageFavoris.style.filter = 'hue-rotate(197deg)';
+        boutonFavoris.addEventListener('click', function() {
+            const vurl=location.href;
+            const vetab = document.getElementById('libelle-formation-affectation').childNodes[0].nodeValue.trim().replace(/[\t\r\n ]+/g,' ');
+            const vform = document.getElementById('libelle-formation-affectation').parentNode.getElementsByTagName('h1')[1].textContent;
+            window.open(URL+'#'+vurl+'|'+vform + '|' + vetab);
+        });
+    }
+    else {
+        document.body.classList.add('parcoursup-userscript')
+    }
+})();
